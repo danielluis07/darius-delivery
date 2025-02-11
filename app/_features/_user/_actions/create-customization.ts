3f5be067ada850ffbb5046158870ed2af85659c2
danielluis07 <bombdaniel@hotmail.com>
2025-02-11 17:28:45 -0300

    changed aws env key name

--- app/_features/_user/_actions/create-customization.ts
+++ app/_features/_user/_actions/create-customization.ts
@@ -13,7 +13,7 @@ const s3 = new S3Client({
   region: process.env.AWS_BUCKET_REGION!,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY!,
-    secretAccessKey: process.env.AWS_SECRET_KEY!,
+    secretAccessKey: process.env.AWS_KEY!,
   },
 });
 
