02a753aa3bddde41c70da2604a8f59cc84952ad5
danielluis07 <bombdaniel@hotmail.com>
2025-04-10 10:13:38 -0300

    created s3  helper functions

--- app/_features/_user/_actions/update-category.ts
+++ app/_features/_user/_actions/update-category.ts
@@ -14,86 +14,11 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { revalidatePath } from "next/cache";
 import crypto from "crypto";
 import { and, eq } from "drizzle-orm";
+import { deleteFromS3, uploadImageToS3 } from "@/lib/s3-upload";
 
 type UpdatedData = {
   name: string;
   image?: string | null | undefined;
-};
-
-const s3 = new S3Client({
-  region: process.env.AWS_REGION!,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-  },
-});
-
-const uploadImageToS3 = async (imageFile: File) => {
-  if (!imageFile) {
-    return null;
-  }
-
-  const generateFileName = (bytes = 32) =>
-    crypto.randomBytes(bytes).toString("hex");
-
-  const arrayBuffer = await imageFile.arrayBuffer();
-  const buffer = Buffer.from(arrayBuffer);
-  const hash = crypto.createHash("sha256");
-  hash.update(buffer);
-  const hashHex = hash.digest("hex");
-
-  const fileName = generateFileName();
-  const putObjectCommand = new PutObjectCommand({
-    Bucket: process.env.AWS_BUCKET_NAME!,
-    Key: fileName,
-    ContentType: imageFile.type,
-    ContentLength: imageFile.size,
-    ChecksumSHA256: hashHex,
-  });
-
-  const signedURL = await getSignedUrl(s3, putObjectCommand, {
-    expiresIn: 3600,
-  });
-
-  const res = await fetch(signedURL, {
-    method: "PUT",
-    body: imageFile,
-    headers: {
-      "Content-Type": imageFile.type,
-    },
-  });
-
-  if (!res.ok) {
-    return null;
-  }
-
-  return signedURL.split("?")[0];
-};
-
-const deleteFromS3 = async (fileName: string) => {
-  try {
-    if (!fileName) {
-      throw new Error("Nome do arquivo não informado");
-    }
-
-    const command = new DeleteObjectCommand({
-      Bucket: process.env.AWS_BUCKET_NAME!,
-      Key: fileName,
-    });
-
-    await s3.send(command);
-
-    return {
-      success: true,
-      message: "Arquivo deletado com sucesso do S3",
-    };
-  } catch (error) {
-    console.error("Erro ao deletar arquivo do S3:", error);
-    return {
-      success: false,
-      message: "Erro ao deletar arquivo do S3",
-    };
-  }
 };
 
 export const updateCategory = async (
