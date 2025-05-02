1b1b7b6a26f8588cedc689345a77b4d35081319e
danielluis07 <bombdaniel@hotmail.com>
2025-05-02 00:16:57 -0300

    fixed import

--- hooks/use-receipt-dialog.ts
+++ hooks/use-receipt-dialog.ts
@@ -1,6 +1,11 @@
-import { Receipt } from "@/app/_features/_user/_components/_receipts/client";
+import { client } from "@/lib/hono";
+import { InferResponseType } from "hono";
 import { create } from "zustand";
 
+type Receipt = InferResponseType<
+  (typeof client.api.receipts.user)[":userId"]["$get"],
+  200
+>["data"];
 interface DialogState {
   isOpen: boolean;
   receipt: Receipt[number] | null;
